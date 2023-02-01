2603f7a3725bd10abeba62788572d22a50b15f96
DaniloNogueira-Silva <dansilva730x@gmail.com>
2023-02-01 16:58:06 -0300

    style: Criar barra de navagação

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,10 +1,45 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import classes from './navbar.module.css';
+import { AiOutlineUser, AiOutlineShoppingCart } from 'react-icons/ai';
 
 const Navbar = () => {
   return (
-    <div>
-      Nav
+    <div className={classes.container}>
+      <div className={classes.wrapper}>
+        <div className={classes.left}>
+          <Link to={'/'} className={classes.title}>
+            Tech Pizza
+          </Link>
+        </div>
+        <div className={classes.center}>
+          <ul className={classes.list}>
+            <li className={classes.listItem}>
+              <a href="#">Home</a>
+            </li>
+            <li className={classes.listItem}>
+              <a href="#contacts">Contato</a>
+            </li>
+            <li className={classes.listItem}>
+              <a href="#foods">Pizzas</a>
+            </li>
+            <li className={classes.listItem}>
+              <a href="#FAQ">FAQ</a>
+            </li>
+            <li className={classes.listItem}>
+              <Link to={'/create'}>Criar</Link>
+            </li>
+          </ul>
+        </div>
+        <div className={classes.right}>
+          <AiOutlineUser className={classes.userIcon}/>
+          <Link to='/cart' className={classes.cartContainer}>
+            <AiOutlineShoppingCart className={classes.cartIcon} />
+            <div className={classes.cartQuantity}>0</div>
+          </Link>
+          <button className={classes.logout}>Sair</button>
+        </div>
+      </div>
     </div>
   )
 }
