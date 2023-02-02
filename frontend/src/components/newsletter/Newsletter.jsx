4bf50a75f15ec605eb50f8b11278a1d2736230fc
DaniloNogueira-Silva <dansilva730x@gmail.com>
2023-02-02 12:46:14 -0300

    style: Criar newsletter

--- frontend/src/components/newsletter/Newsletter.jsx
+++ frontend/src/components/newsletter/Newsletter.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import classes from './newsletter.module.css';
+import { AiOutlineSend } from 'react-icons/ai';
+import newsletterIllustration from '../../assets/get-newsletter-updates.svg';
 
 const Newsletter = () => {
   return (
-    <div>
-      
-    </div>
+    <section id='contacts' className={classes.container}>
+      <div className={classes.wrapper}>
+        <h4 className={classes.subtitle}>Receba as últimas ofertas</h4>
+        <h2 className={classes.title}>Newsletter</h2>
+        <div className={classes.inputContainer}>
+          <input type="text" placeholder='Digite seu email...' />
+          <AiOutlineSend className={classes.sendIcon} />
+        </div>
+        <img src={newsletterIllustration} className={classes.illustration} alt="" />
+      </div>
+    </section>
   )
 }
 
