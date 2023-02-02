555ab363d10005165344a0d60da9f2cc90abb313
DaniloNogueira-Silva <dansilva730x@gmail.com>
2023-02-02 09:54:14 -0300

    style: Criar categoria de comidas

--- frontend/src/components/foods/Foods.jsx
+++ frontend/src/components/foods/Foods.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { foodTypes } from '../../data/data';
 import classes from './foods.module.css';
+import { Link } from 'react-router-dom';
 
 const Foods = () => {
   return (
-    <div>
-      Foods
-    </div>
+    <section id='foods' className={classes.container}>
+      <div className={classes.wrapper}>
+        <h4 className={classes.subtitle}>O quê temos para vocês?</h4>
+        <h2 className={classes.title}>As melhores refeições da cidade</h2>
+        <div className={classes.foods}>
+          {foodTypes.map((foodType) => (
+            <Link to={`/foods/${foodType.name}`} key={foodType.id} className={classes.food}>
+              <h4>{foodType.name}</h4>
+              <div className={classes.imgContainer}>
+                <img src={foodType.img}/>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </section>
   )
 }
 
