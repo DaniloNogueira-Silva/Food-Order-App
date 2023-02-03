db75a453ebb6ab3ae46d6af24c643ce50e4cbcaa
DaniloNogueira-Silva <dansilva730x@gmail.com>
2023-02-03 08:03:47 -0300

    style: Criar checkout page

--- frontend/src/components/checkout/Checkout.jsx
+++ frontend/src/components/checkout/Checkout.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import classes from './checkout.module.css';
+import React from 'react'
+import { useSelector } from 'react-redux'
+import classes from './checkout.module.css'
 
 const Checkout = () => {
+  const {products} = useSelector((state) => state.cart)
+
+  let totalPrice = 0
+  products.map((product) => totalPrice += (product.quantity * product.price))
   return (
-    <div>
-      Checkout
+    <div className={classes.container}>
+      <div className={classes.wrapper}>
+        <h2>Your order is successful</h2>
+        <p>Expect it in 1 hour</p>
+        <span>Total Price: ${totalPrice}</span>
+      </div>
     </div>
   )
 }
